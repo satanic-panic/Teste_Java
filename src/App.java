f0f109cd666c2aaa09985a79dd870da7846f1413
David Samael <davidsamaelsp@gmail.com>
2025-05-29 11:30:49 +0100

    29-05-2025 0.2

--- src/App.java
+++ src/App.java
@@ -4,7 +4,11 @@ public class App {
     }
 
     public static String returnString(int i) {
-        return String.valueOf(i);
+        if (i % 3 == 0 && i % 5 == 0) {
+            return "ITClinical";
+        } else {
+            return String.valueOf(i);
+        }
     }
 
 }
--- src/AppTest.java
+++ src/AppTest.java
@@ -9,4 +9,11 @@ public class AppTest {
         assertEquals("ITClinical", app.returnString(15));
         assertEquals("ITClinical", app.returnString(30));
     }
+
+    @Test
+    public void testReturnString_MultipleOf3() {
+      App app = new App();
+      assertEquals("IT", app.returnString(3));
+      assertEquals("IT", app.returnString(9));
+    }
 }
