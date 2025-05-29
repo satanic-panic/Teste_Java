56d39d26715e5b2bd7ddcee0356771948d9fd6ae
David Samael <davidsamaelsp@gmail.com>
2025-05-29 11:48:24 +0100

    29-05-2025 0.7

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
-    public static void main() {
+    public static void main(String[] args) {
         for (int i = 1; i <= 100; i++) {
             System.out.print(returnString(i));
         }
