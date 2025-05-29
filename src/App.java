6d7cafbbcd498d2db3cebc44a01999cecc018440
David Samael <davidsamaelsp@gmail.com>
2025-05-29 11:32:31 +0100

    29-05-2025 0.3

--- src/App.java
+++ src/App.java
@@ -6,7 +6,10 @@ public class App {
     public static String returnString(int i) {
         if (i % 3 == 0 && i % 5 == 0) {
             return "ITClinical";
-        } else {
+        } else if (i % 3 == 0) {
+            return "IT";
+        }
+        else {
             return String.valueOf(i);
         }
     }
